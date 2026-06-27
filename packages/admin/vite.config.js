94362a50fa3c4eeca4d5fb65042015be47223227
irealby3d <irealby3d@gmail.com>
2026-06-28 02:32:27 +0500

    Update vite.config.js

--- packages/admin/vite.config.js
+++ packages/admin/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // <--- SHU QATORNI QO'SHISH SHART!
   plugins: [react()],
 })
